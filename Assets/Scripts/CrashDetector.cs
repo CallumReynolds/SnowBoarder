ca2fc72ac5f7cc6deff1966550baad00ffad0fdf
CallumReynolds <callum_rey@hotmail.com>
2024-01-16 20:50:12 +1000

    particle effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,15 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 0.5f;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ground")
         {
-            SceneManager.LoadScene("SampleScene");
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,11 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 1f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player") 
         {
-            SceneManager.LoadScene("SampleScene");            
+            Invoke("ReloadScene", reloadDelay);            
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
